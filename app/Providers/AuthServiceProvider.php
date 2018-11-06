954589b169998c5728bb8bb25b597078d54070ad
Markus Koehler <info@koehler.pro>
2018-11-06 13:06:57 +0100

    add passport & cors

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Route;
+use Laravel\Passport\Passport;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -25,6 +27,12 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        //Passport::routes();
+
+        Route::group(['middleware'=> 'cors'], function() {
+            Passport::routes();
+        });
+
+        Passport::enableImplicitGrant();
     }
 }
